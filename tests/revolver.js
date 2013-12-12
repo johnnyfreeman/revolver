141916a442f1e24c0e265cbbc4f7a8ba50ad9420
Johnny Freeman <prsjohnny@gmail.com>
2013-12-12 13:35:45 -0500

    added assertion for checking the transitions namespace. using revolver package.

--- tests/revolver.js
+++ tests/revolver.js
@@ -1,13 +1,17 @@
 define([
     'intern!object',
     'intern/chai!assert',
-    'intern/order!../js/revolver.js'
+    'revolver/revolver'
 ], function (registerSuite, assert) {
+
   registerSuite({
-    name: 'revolver',
+    name: 'revolver.js',
 
     registerTransition: function () {
-      assert.strictEqual(Revolver.registerTransition('test', function () {}), Revolver, 'Revolver.registerTransition should return the global Revolver object.');
+      var handler = function () {};
+      var result = Revolver.registerTransition('test', handler);
+      assert.strictEqual(Revolver.transitions['test'], handler, 'Revolver.registerTransition should save the handler in the Revolver.transitions namespace.');
+      assert.strictEqual(result, Revolver, 'Revolver.registerTransition should return the Revolver global object.');
     }
   });
 });
