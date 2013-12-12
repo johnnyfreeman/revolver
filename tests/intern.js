fa9eabe5a714e56760ff5e8cb12241da6b999b18
Johnny Freeman <prsjohnny@gmail.com>
2013-12-12 11:50:56 -0500

    reduced the amount of environment revolver has to be tested in while I learn how to get intern/travis/revolver all working together

--- tests/intern.js
+++ tests/intern.js
@@ -22,13 +22,13 @@ define({
 	// OnDemand. Options that will be permutated are browserName, version, platform, and platformVersion; any other
 	// capabilities options specified for an environment will be copied as-is
 	environments: [
-		{ browserName: 'internet explorer', version: '11', platform: 'Windows 8.1' },
-		{ browserName: 'internet explorer', version: '10', platform: 'Windows 8' },
-		{ browserName: 'internet explorer', version: '9', platform: 'Windows 7' },
-		{ browserName: 'firefox', version: '25', platform: [ 'OS X 10.6', 'Windows 7' ] },
-		{ browserName: 'firefox', version: '24', platform: 'Linux' },
-		{ browserName: 'chrome', version: '', platform: [ 'Linux', 'OS X 10.6', 'Windows 7' ] },
-		{ browserName: 'safari', version: '6', platform: 'OS X 10.8' }
+		// { browserName: 'internet explorer', version: '11', platform: 'Windows 8.1' },
+		// { browserName: 'internet explorer', version: '10', platform: 'Windows 8' },
+		// { browserName: 'internet explorer', version: '9', platform: 'Windows 7' },
+		// { browserName: 'firefox', version: '25', platform: [ 'OS X 10.6', 'Windows 7' ] },
+		// { browserName: 'firefox', version: '24', platform: 'Linux' },
+		{ browserName: 'chrome', version: '', platform: [ 'Linux', 'OS X 10.6', 'Windows 7' ] }// ,
+		// { browserName: 'safari', version: '6', platform: 'OS X 10.8' }
 	],
 
 	// Maximum number of simultaneous integration tests that should be executed on the remote WebDriver service
