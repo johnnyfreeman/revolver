bd92a8804eeba414acb81a6ac9b7352986ec0a3c
Johnny Freeman <prsjohnny@gmail.com>
2013-12-18 17:47:41 -0500

    cleaned up gruntfile

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,25 +1,18 @@
 'use strict';
 
 module.exports = function(grunt) {
-
-  // Project configuration.
   grunt.initConfig({
 
     pkg: grunt.file.readJSON('package.json'),
-
-    intern: {
-      client: {
-        options: {
-//          runType: 'runner', // defaults to 'client' (node.js)
-          // sauceUsername: 'revolverjs', // don't want to use sauce labs with grunt
-          // sauceAccessKey: 'c0408be0-b8ea-4245-b2de-29ccb698c7d8',
-          config: 'tests/intern',
-          // reporters: [ 'console', 'lcov' ], // defaults to console
-          // suites: [ 'tests/revolver' ] // this is specified in tests/intern
-        }
-      }
+    
+    // mocha testing
+    mocha: {
+      test: {
+        src: ['tests/**/*.html'],
+      },
     },
-
+    
+    // coffeescript compilation
     coffee: {
       compileWithMaps: {
         options: {
@@ -31,7 +24,8 @@ module.exports = function(grunt) {
         }
       }
     },
-
+    
+    // minification
     uglify: {
       options: {
         banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n',
@@ -45,10 +39,9 @@ module.exports = function(grunt) {
         }
       }
     },
-
-    // regarde configuration
+    
+    // watch server
     regarde: {
-
       coffee: {
         files: ['coffee/revolver.coffee'],
         tasks: 'coffee'
@@ -57,8 +50,12 @@ module.exports = function(grunt) {
       uglify: {
         files: ['js/revolver.js'],
         tasks: 'uglify'
+      },
+      
+      test: {
+        files: ['js/revolver.js', 'js/revolver.min.js'],
+        tasks: 'test'
       }
-
     }
 
   });
@@ -67,13 +64,10 @@ module.exports = function(grunt) {
   grunt.loadNpmTasks('grunt-contrib-uglify');
   grunt.loadNpmTasks('grunt-contrib-coffee');
   grunt.loadNpmTasks('grunt-regarde');
-  grunt.loadNpmTasks('intern')
+  grunt.loadNpmTasks('grunt-mocha');
 
-  // Register a test task that uses Intern
-  grunt.registerTask('test', [ 'intern' ]);
-
-  // Default tasks
-  grunt.registerTask('default', ['coffee', 'uglify']);
-  grunt.registerTask('server', ['regarde']);
-
+  // Custom tasks
+  grunt.registerTask('default', 'build');
+  grunt.registerTask('server', 'regarde');
+  grunt.registerTask('build', ['coffee', 'uglify', 'mocha']);
 };
