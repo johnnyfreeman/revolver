12a0104bff067754ece403f62e53a2415a5c7e98
Johnny Freeman <prsjohnny@gmail.com>
2014-05-14 13:39:05 -0400

    added goTo test and tidied things up a bit

--- src/tests.coffee
+++ src/tests.coffee
@@ -1,102 +1,86 @@
-suite "Instance Methods", ->
+suite 'Instance Methods', ->
   
   # revolver instance
   slider = undefined
   originalSlider = undefined
   setup ->
-    slider = new Revolver(
-      containerSelector: "#myslider"
-      slidesSelector: ".slide"
-    )
+    slider = new Revolver
+      containerSelector: '#myslider'
+      slidesSelector: '.slide'
     originalSlider = _.clone(slider)
-
   suiteTeardown ->
     slider = originalSlider = null
 
   
   # addSlide
-  suite "#addSlide()", ->
-    test "adds one more to this.slides array", ->
+  suite '#addSlide()', ->
+    test 'adds one more to this.slides array', ->
       numSlides = slider.slides.length
-      slider.addSlide document.createElement("div")
+      slider.addSlide document.createElement('div')
       assert.strictEqual slider.slides.length, numSlides + 1
-
-    test "recalculates this.numSlides correctly", ->
-      slider.addSlide document.createElement("div")
+    test 'recalculates this.numSlides correctly', ->
+      slider.addSlide document.createElement('div')
       assert.strictEqual slider.numSlides, slider.slides.length
-
-    test "recalculates this.nextSlide correctly", ->
+    test 'recalculates this.nextSlide correctly', ->
       nextSlide = (if slider.currentSlide is slider.lastSlide then 0 else slider.currentSlide + 1)
-      slider.addSlide document.createElement("div")
+      slider.addSlide document.createElement('div')
       assert.strictEqual slider.nextSlide, nextSlide
-
 
   
   # changeStatus
-  suite "#changeStatus()", ->
-    test "plays", ->
-      slider.changeStatus "playing"
+  suite '#changeStatus()', ->
+    test 'plays', ->
+      slider.changeStatus 'playing'
       assert.strictEqual slider.status.playing, true
       assert.strictEqual slider.status.paused, false
       assert.strictEqual slider.status.stopped, false
-
-    test "pauses", ->
-      slider.changeStatus "paused"
+    test 'pauses', ->
+      slider.changeStatus 'paused'
       assert.strictEqual slider.status.playing, false
       assert.strictEqual slider.status.paused, true
       assert.strictEqual slider.status.stopped, false
-
-    test "stops", ->
-      slider.changeStatus "stopped"
+    test 'stops', ->
+      slider.changeStatus 'stopped'
       assert.strictEqual slider.status.playing, false
       assert.strictEqual slider.status.paused, false
       assert.strictEqual slider.status.stopped, true
 
-
   
   # pause
-  suite "#pause()", ->
-    test "changes status to paused if playing or stopped", ->
-      
+  suite '#pause()', ->
+    test 'changes status to paused if playing or stopped', ->
       # from stopped
       slider.stop()
       slider.pause()
       assert.strictEqual slider.status.paused, true
-      
       # from playing
       slider.play()
       slider.pause()
       assert.strictEqual slider.status.paused, true
-
-    test "this.intervalId is null", ->
+    test 'this.intervalId is null', ->
       slider.pause()
       assert.strictEqual slider.intervalId, null
 
-
   
   # play
-  suite "#play()", ->
-    test "changes status to playing if paused or stopped", ->
-      
+  suite '#play()', ->
+    test 'changes status to playing if paused or stopped', ->
       # from stopped
       slider.stop()
       slider.play()
       assert.strictEqual slider.status.playing, true
-      
       # from paused
       slider.pause()
       slider.play()
       assert.strictEqual slider.status.playing, true
-
-    test "this.intervalId isnt null", ->
+    test 'this.intervalId isnt null', ->
       slider.play()
       assert.notStrictEqual slider.intervalId, null
 
 
-  
   # reset
-  suite "#reset()", ->
-    test "only sets this.nextSlide to 0 if this.currentSlide doesnt equal 0", ->
+  suite '#reset()', ->
+    test 'only sets this.nextSlide to 0 if this.currentSlide doesnt equal 0', ->
       slider.pause()
       slider.reset()
       assert.notStrictEqual slider.nextSlide, 0
@@ -104,67 +88,76 @@ suite "Instance Methods", ->
       slider.reset()
       assert.strictEqual slider.nextSlide, 0
 
-
   
   # restart
-  suite "#restart()", ->
+  suite '#restart()', ->
     setup ->
       slider.restart()
-
-    
     #    test('this.currentSlide equals 0', function () {
     #      assert.strictEqual(slider.currentSlide, 0);
     #    });
-    test "slider is playing", ->
+    test 'slider is playing', ->
       assert.strictEqual slider.status.playing, true
       assert.strictEqual slider.status.paused, false
       assert.strictEqual slider.status.stopped, false
 
-
   
   # setOptions
-  suite "#setOptions()", ->
-    test "sets a new option", ->
-      slider.setOptions foo: "bar"
-      assert.strictEqual slider.options.foo, "bar"
-
-    test "overrides existing options", ->
-      slider.setOptions slidesSelector: ".mySlides"
-      assert.strictEqual slider.options.slidesSelector, ".mySlides"
-
+  suite '#setOptions()', ->
+    test 'sets a new option', ->
+      slider.setOptions foo: 'bar'
+      assert.strictEqual slider.options.foo, 'bar'
+    test 'overrides existing options', ->
+      slider.setOptions slidesSelector: '.mySlides'
+      assert.strictEqual slider.options.slidesSelector, '.mySlides'
 
   
   # stop
-  suite "#stop()", ->
-    test "changes status to stopped if paused or playing", ->
-      
+  suite '#stop()', ->
+    test 'changes status to stopped if paused or playing', ->
       # from playing
       slider.play()
       slider.stop()
       assert.strictEqual slider.status.stopped, true
-      
       # from paused
       slider.pause()
       slider.stop()
       assert.strictEqual slider.status.stopped, true
-
-    test "this.intervalId is null", ->
+    test 'this.intervalId is null', ->
       slider.stop()
       assert.strictEqual slider.intervalId, null
 
-
+  
+  # stop
+  suite '#goTo()', ->
+    test 'does nothing if disabled', ->
+      slider.goTo 0
+      slider.disabled = true
+      slider.goTo slider.lastSlide
+      assert.strictEqual slider.currentSlide, 0
+    test 'goes to intended slide', ->
+      nextSlide = slider.nextSlide
+      slider.goTo nextSlide
+      assert.strictEqual slider.currentSlide, nextSlide
 
 
 # Static Methods
-suite "Static Methods", ->
+suite 'Static Methods', ->
   
   # registerTranistion
-  suite "#registerTransition()", ->
+  suite '#registerTransition()', ->
     handler = ->
-
-    result = Revolver.registerTransition("test", handler)
-    test "saves the handler in the transitions namespace", ->
+    result = Revolver.registerTransition('test', handler)
+    test 'saves the handler in the transitions namespace', ->
       assert.strictEqual Revolver.transitions.test, handler
-
-    test "returns the Revolver global object", ->
+    test 'returns the Revolver global object', ->
       assert.strictEqual result, Revolver
+  
+  # setSelectorEngine
+  # suite '#setSelectorEngine()', ->
+  #   new$ = ->
+  #   old$ = Revolver.$
+  #   Revolver.setSelectorEngine(new$)
+  #   test 'saves the handler in the $ namespace', ->
+  #     assert.strictEqual Revolver.$, new$
+  #   Revolver.$ = old$
--- test/tests.js
+++ test/tests.js
@@ -1,58 +1,58 @@
 (function() {
-  suite("Instance Methods", function() {
+  suite('Instance Methods', function() {
     var originalSlider, slider;
     slider = void 0;
     originalSlider = void 0;
     setup(function() {
       slider = new Revolver({
-        containerSelector: "#myslider",
-        slidesSelector: ".slide"
+        containerSelector: '#myslider',
+        slidesSelector: '.slide'
       });
       return originalSlider = _.clone(slider);
     });
     suiteTeardown(function() {
       return slider = originalSlider = null;
     });
-    suite("#addSlide()", function() {
-      test("adds one more to this.slides array", function() {
+    suite('#addSlide()', function() {
+      test('adds one more to this.slides array', function() {
         var numSlides;
         numSlides = slider.slides.length;
-        slider.addSlide(document.createElement("div"));
+        slider.addSlide(document.createElement('div'));
         return assert.strictEqual(slider.slides.length, numSlides + 1);
       });
-      test("recalculates this.numSlides correctly", function() {
-        slider.addSlide(document.createElement("div"));
+      test('recalculates this.numSlides correctly', function() {
+        slider.addSlide(document.createElement('div'));
         return assert.strictEqual(slider.numSlides, slider.slides.length);
       });
-      return test("recalculates this.nextSlide correctly", function() {
+      return test('recalculates this.nextSlide correctly', function() {
         var nextSlide;
         nextSlide = (slider.currentSlide === slider.lastSlide ? 0 : slider.currentSlide + 1);
-        slider.addSlide(document.createElement("div"));
+        slider.addSlide(document.createElement('div'));
         return assert.strictEqual(slider.nextSlide, nextSlide);
       });
     });
-    suite("#changeStatus()", function() {
-      test("plays", function() {
-        slider.changeStatus("playing");
+    suite('#changeStatus()', function() {
+      test('plays', function() {
+        slider.changeStatus('playing');
         assert.strictEqual(slider.status.playing, true);
         assert.strictEqual(slider.status.paused, false);
         return assert.strictEqual(slider.status.stopped, false);
       });
-      test("pauses", function() {
-        slider.changeStatus("paused");
+      test('pauses', function() {
+        slider.changeStatus('paused');
         assert.strictEqual(slider.status.playing, false);
         assert.strictEqual(slider.status.paused, true);
         return assert.strictEqual(slider.status.stopped, false);
       });
-      return test("stops", function() {
-        slider.changeStatus("stopped");
+      return test('stops', function() {
+        slider.changeStatus('stopped');
         assert.strictEqual(slider.status.playing, false);
         assert.strictEqual(slider.status.paused, false);
         return assert.strictEqual(slider.status.stopped, true);
       });
     });
-    suite("#pause()", function() {
-      test("changes status to paused if playing or stopped", function() {
+    suite('#pause()', function() {
+      test('changes status to paused if playing or stopped', function() {
         slider.stop();
         slider.pause();
         assert.strictEqual(slider.status.paused, true);
@@ -60,13 +60,13 @@
         slider.pause();
         return assert.strictEqual(slider.status.paused, true);
       });
-      return test("this.intervalId is null", function() {
+      return test('this.intervalId is null', function() {
         slider.pause();
         return assert.strictEqual(slider.intervalId, null);
       });
     });
-    suite("#play()", function() {
-      test("changes status to playing if paused or stopped", function() {
+    suite('#play()', function() {
+      test('changes status to playing if paused or stopped', function() {
         slider.stop();
         slider.play();
         assert.strictEqual(slider.status.playing, true);
@@ -74,13 +74,13 @@
         slider.play();
         return assert.strictEqual(slider.status.playing, true);
       });
-      return test("this.intervalId isnt null", function() {
+      return test('this.intervalId isnt null', function() {
         slider.play();
         return assert.notStrictEqual(slider.intervalId, null);
       });
     });
-    suite("#reset()", function() {
-      return test("only sets this.nextSlide to 0 if this.currentSlide doesnt equal 0", function() {
+    suite('#reset()', function() {
+      return test('only sets this.nextSlide to 0 if this.currentSlide doesnt equal 0', function() {
         slider.pause();
         slider.reset();
         assert.notStrictEqual(slider.nextSlide, 0);
@@ -89,32 +89,32 @@
         return assert.strictEqual(slider.nextSlide, 0);
       });
     });
-    suite("#restart()", function() {
+    suite('#restart()', function() {
       setup(function() {
         return slider.restart();
       });
-      return test("slider is playing", function() {
+      return test('slider is playing', function() {
         assert.strictEqual(slider.status.playing, true);
         assert.strictEqual(slider.status.paused, false);
         return assert.strictEqual(slider.status.stopped, false);
       });
     });
-    suite("#setOptions()", function() {
-      test("sets a new option", function() {
+    suite('#setOptions()', function() {
+      test('sets a new option', function() {
         slider.setOptions({
-          foo: "bar"
+          foo: 'bar'
         });
-        return assert.strictEqual(slider.options.foo, "bar");
+        return assert.strictEqual(slider.options.foo, 'bar');
       });
-      return test("overrides existing options", function() {
+      return test('overrides existing options', function() {
         slider.setOptions({
-          slidesSelector: ".mySlides"
+          slidesSelector: '.mySlides'
         });
-        return assert.strictEqual(slider.options.slidesSelector, ".mySlides");
+        return assert.strictEqual(slider.options.slidesSelector, '.mySlides');
       });
     });
-    return suite("#stop()", function() {
-      test("changes status to stopped if paused or playing", function() {
+    suite('#stop()', function() {
+      test('changes status to stopped if paused or playing', function() {
         slider.play();
         slider.stop();
         assert.strictEqual(slider.status.stopped, true);
@@ -122,22 +122,36 @@
         slider.stop();
         return assert.strictEqual(slider.status.stopped, true);
       });
-      return test("this.intervalId is null", function() {
+      return test('this.intervalId is null', function() {
         slider.stop();
         return assert.strictEqual(slider.intervalId, null);
       });
     });
+    return suite('#goTo()', function() {
+      test('does nothing if disabled', function() {
+        slider.goTo(0);
+        slider.disabled = true;
+        slider.goTo(slider.lastSlide);
+        return assert.strictEqual(slider.currentSlide, 0);
+      });
+      return test('goes to intended slide', function() {
+        var nextSlide;
+        nextSlide = slider.nextSlide;
+        slider.goTo(nextSlide);
+        return assert.strictEqual(slider.currentSlide, nextSlide);
+      });
+    });
   });
 
-  suite("Static Methods", function() {
-    return suite("#registerTransition()", function() {
+  suite('Static Methods', function() {
+    return suite('#registerTransition()', function() {
       var handler, result;
       handler = function() {};
-      result = Revolver.registerTransition("test", handler);
-      test("saves the handler in the transitions namespace", function() {
+      result = Revolver.registerTransition('test', handler);
+      test('saves the handler in the transitions namespace', function() {
         return assert.strictEqual(Revolver.transitions.test, handler);
       });
-      return test("returns the Revolver global object", function() {
+      return test('returns the Revolver global object', function() {
         return assert.strictEqual(result, Revolver);
       });
     });
