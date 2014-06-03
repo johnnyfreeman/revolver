f2adb15fc9f7c7b5c484c8d76a7f8888f97e5ef9
Johnny Freeman <prsjohnny@gmail.com>
2014-06-03 17:20:31 -0400

    added ready event

--- examples/events/index.js
+++ examples/events/index.js
@@ -1,8 +1,24 @@
 // use Sizzle for greater browser compatability
 Revolver.setSelectorEngine($.find);
 
+var $events = $('.events');
+var events = _(['pause', 'play', 'start', 'stop', 'ready', 'restart', 'transitionStart', 'transitionComplete']);
+var logEvent = function (eventName) {
+  $events.prepend('<li class="event"><span class="datetime">'+new Date()+':</span> <a href="https://github.com/johnnyfreeman/revolver/blob/master/api/events/'+eventName.toLowerCase()+'.md">'+eventName+'</a> fired</li>');
+};
+
 // Start it up
-var mySlider = new Revolver({containerSelector: '.slider', slidesSelector: '.slide', transition: {name: 'slide'}});
+var mySlider = new Revolver({
+  containerSelector: '.slider',
+  onReady: logEvent.bind(this, 'ready'),
+  slidesSelector: '.slide',
+  transition: {name: 'slide'}
+});
+
+// regist event handlers for each event
+events.each(function (eventName) {
+  mySlider.on(eventName, logEvent.bind(this, eventName));
+});
 
 /*************************************
  * API Controls
@@ -13,15 +29,4 @@ $(document).on('click', '.call', function (e) {
   var method = $el.data('method');
   var argument = $el.data('argument');
   mySlider[method](argument);
-});
-
-/*************************************
- * Event log
- ************************************/
-var $events = $('.events');
-var events = _(['pause', 'play', 'start', 'stop', 'ready', 'restart', 'transitionStart', 'transitionComplete']);
-events.each(function (eventName) {
-  mySlider.on(eventName, function () {
-    $events.prepend('<li class="event"><span class="datetime">'+new Date()+':</span> <a href="https://github.com/johnnyfreeman/revolver/blob/master/api/events/'+eventName.toLowerCase()+'.md">'+eventName+'</a> fired</li>');
-  });
 });
